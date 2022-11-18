060ee13939201cc3fe04c893942c5c293d4e779e
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-18 15:22:16 +0100

    test create Cuffie product (true)

--- src/generation/italy/shop/Main.java
+++ src/generation/italy/shop/Main.java
@@ -4,5 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// creazio prodotti casuali
+		
+		//cuffie
+		//String nomeProduct, String brand, int price, String colore, boolean isWireless 
+		Cuffie headphone = new Cuffie("AirPods" , "Apple", 120, "white", true);
+		System.out.println(headphone);
+		System.out.println("----------------------");
 	}
 }
