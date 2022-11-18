4265276e9f9beeeb56d5ee8bc9e41364bd498f3f
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-18 15:02:52 +0100

    create a class Smartphone

--- src/generation/italy/shop/Smarphone.java
+++ src/generation/italy/shop/Smarphone.java
@@ -1,5 +1,35 @@
 package generation.italy.shop;
 
-public class Smarphone {
+public class Smarphone extends Prodotto{
+	
+	// variabili d'istanza
+		private String codImei;
+		private int rom;
+		
+		public Smarphone(int codeProduct, String nomeProduct, String brand, int price, String codImei, int rom ) {
+			
+			// chiamo il costruttore della classe Prodotto
+			super(codeProduct, nomeProduct, brand, price);
+			this.codImei = codImei;
+			this.rom = rom;
+		}
+		
+		// metodi get e set
+		public String getCodImei() {
+			return codImei;
+		}
 
+		public void setCodImei(String codImei) {
+			this.codImei = codImei;
+		}
+
+		public int getRom() {
+			return rom;
+		}
+
+		public void setRom(int rom) {
+			this.rom = rom;
+		}
+
+		
 }
