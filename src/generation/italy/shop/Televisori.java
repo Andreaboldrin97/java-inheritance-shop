4f82af0c8ab03b0be540dd7088688a71440b01db
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-18 15:06:14 +0100

    create a class Televisori

--- src/generation/italy/shop/Televisori.java
+++ src/generation/italy/shop/Televisori.java
@@ -1,5 +1,37 @@
 package generation.italy.shop;
 
-public class Televisori {
+//SUB CLASS
+//la classe Televisori estente la classe prodotto
 
+public class Televisori extends Prodotto{
+
+	// variabili d'istanza
+			private int tvSize;
+			private boolean isSmartTv;
+			
+			public Televisori(int codeProduct, String nomeProduct, String brand, int price, int tvSize, boolean isSmartTv) {
+				
+				// chiamo il costruttore della classe Prodotto
+				super(codeProduct, nomeProduct, brand, price);
+				this.tvSize = tvSize;
+				this.isSmartTv = isSmartTv;
+			}
+			
+			// metodi get e set
+			public int getTvSize() {
+				return tvSize;
+			}
+
+			public void setTvSize(int tvSize) {
+				this.tvSize = tvSize;
+			}
+
+			public boolean isSmartTv() {
+				return isSmartTv;
+			}
+
+			public void setSmartTv(boolean isSmartTv) {
+				this.isSmartTv = isSmartTv;
+			}
+			
 }
